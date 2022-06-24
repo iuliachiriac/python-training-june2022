0bd6509dd4cfaf02473886f20d04e28ed985111b
Iulia Chiriac <iulia.chiriac@yahoo.com>
2022-06-24 08:52:00 +0200

    Add solutions

--- examples/oop.py
+++ examples/oop.py
@@ -6,15 +6,20 @@ class Person:
 
     count = 0  # class variable
 
-    def __init__(self, name):
+    def __init__(self, name, date_of_birth):
         self.name = name  # instance variables
-        Person.count += 1
+        self.date_of_birth = date_of_birth
+        self._increment_count()
+
+    @property
+    def age(self):
+        return self.compute_age(self.date_of_birth)
 
     def greet(self, greeting):  # instance method
         print(f'{self.name} says "{greeting}!"')
 
     @classmethod
-    def increment_count(cls):  # class method
+    def _increment_count(cls):  # class method
         cls.count += 1
 
     @staticmethod
@@ -22,13 +27,27 @@ class Person:
         diff = date.today() - date_of_birth
         return int(diff.days / 365.25)
 
+    def __str__(self):
+        return f'{self.__class__.__name__} object (name={self.name})'
+
+
+class Student(Person):
+    count = 0
+
+    def __init__(self, name, university, date_of_birth):
+        super().__init__(name, date_of_birth)
+        self.university = university
+
+    def greet(self):
+        super().greet("Good morning")
+
 
 if __name__ == '__main__':
-    p1 = Person('Anna')  # __init__ is executed
+    p1 = Person('Anna', date(2001, 5, 7))  # __init__ is executed
 
     print('Number of persons:', Person.count)
 
-    p2 = Person('John')
+    p2 = Person('John', date(1986, 2, 23))
     print(p1.name, p2.name)
 
     print('Number of persons:', Person.count)
@@ -39,11 +58,21 @@ if __name__ == '__main__':
     p1.greet('Hello')
     p2.greet('Hi')
 
-    Person.increment_count()
+    Person._increment_count()
     print('Number of persons:', Person.count)
     print(Person.compute_age(date(1999, 3, 1)))
 
-    p1.increment_count()
+    p1._increment_count()
     print('Number of persons:', Person.count)
 
     print(p1.compute_age(date(2006, 3, 1)))
+
+    print(p1, str(p1), repr(p1))
+
+    print(f"{p1.name}'s age is {p1.age}.")
+
+    s1 = Student('Mike', 'ASE', date(2003, 2, 23))
+    s1.greet()
+    print(s1, s1.university)
+
+    print(Student.count, Person.count)
